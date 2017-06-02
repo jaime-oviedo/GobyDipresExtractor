4cede46e98eba45eac5dc5e3e64f05d7ad05321d
jaime-oviedo <jaime.oviedo@gmail.com>
2017-06-02 00:15:13 -0400

    Se eliminan archivos refactorizados a paquetes específicos.
    Se corrigen errores en el archivo properties
    Se modifica el ClasificadorPresupuestario extractor para que absorba
    como clase intera a clase ClasificadorPresupuestario.

--- src/com/ingenium/goby/extractors/dipres/ClasificadorPresupuestarioExtractor.java
+++ src/com/ingenium/goby/extractors/dipres/ClasificadorPresupuestarioExtractor.java
@@ -13,6 +13,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.logging.Logger;
 
 /**
@@ -22,14 +24,57 @@ import java.util.logging.Logger;
  * @author JaimeRodrigo
  */
 public class ClasificadorPresupuestarioExtractor implements Extractor {
-
+  
+  private final class ClasificadorPresupuestario {
+    
+    private final int subtitulo;
+    private final int item;
+    private final int asignacion;
+    private final String nombre;
+    private final String descripcion;
+    
+    /**
+     * Crea una nueva instancia de la clase ClasificadorPresupuestario.
+     *
+     * @param subtitulo
+     *          el valor del campo subtitulo
+     * @param item
+     *          el valor del campo item
+     * @param asignacion
+     *          el valor del campo asignacion
+     * @param nombre
+     *          el valor del campo nombre
+     * @param descripcion
+     *          el valor del campo descripcion
+     */
+    public ClasificadorPresupuestario(int subtitulo, int item, int asignacion,
+        String nombre, String descripcion) {
+      super();
+      this.subtitulo = subtitulo;
+      this.item = item;
+      this.asignacion = asignacion;
+      this.nombre = nombre;
+      this.descripcion = descripcion;
+    }
+    
+    @Override
+    public String toString() {
+      StringBuilder sb = new StringBuilder();
+      sb.append(String.valueOf(subtitulo)).append(";")
+          .append(String.valueOf(item)).append(";")
+          .append(String.valueOf(asignacion)).append(";").append(nombre)
+          .append(";").append(descripcion).append(";");
+      return sb.toString();
+    }
+  }
+  
   private static final Logger log = Logger.getLogger(
       "com.ingenium.goby.extractors.dipres.ClasificadorPresupuestarioExtractor");
-
+  
   private final String extractionSource;
-
+  
   private final String extractionDestination;
-
+  
   /**
    * Crea una nueva instancia de la clase BudgetClassifierCVStoJSONParser.
    */
@@ -39,13 +84,152 @@ public class ClasificadorPresupuestarioExtractor implements Extractor {
     extractionDestination = Messages
         .getString("ClasificadorPresupuestarioExtractor.destinationUrl"); //$NON-NLS-1$
   }
-
+  
   /*
    * (non-Javadoc)
    * @see com.ingenium.goby.extractors.Extractor#extract()
    */
   @Override
   public void extract() throws ExtractionException {
+    
+    CSVReader reader = getReader();
+    ClasificadorPresupuestarioExtractor.log.fine("Comenzando la extracción");
+    String[] line;
+    try {
+      while ((line = reader.readNext()) != null) {
+        StringBuilder stb = new StringBuilder(400);
+        for (String element : line) {
+          stb.append(element);
+          stb.append("\t");
+        }
+        System.out.println(stb);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      
+    }
+  }
+  
+  /**
+   * Extrae los clasificadores presupuestarios a una colección en un formato
+   * plano.
+   *
+   * @return la colección con los clasificadores extraidos
+   * @throws ExtractionException
+   *           en caso de que no puedan extraerse los clasificadores por
+   *           problemas en el archivo fuente
+   **/
+  
+  Collection<ClasificadorPresupuestario> extractClasificadoresPresupuestarios()
+      throws ExtractionException {
+    
+    CSVReader reader = getReader();
+    
+    ClasificadorPresupuestarioExtractor.log.fine("Comenzando la extracción");
+    Collection<ClasificadorPresupuestario> clasificadores = new ArrayList<>();
+    String[] line;
+    int cnumber = 0;
+    try {
+      while ((line = reader.readNext()) != null) {
+        System.out.println("Procesando item " + cnumber);
+        ArrayList<String> elements = new ArrayList<>();
+        int index = 0;
+        for (String element : line) {
+          elements.add(index, element);
+          index++;
+        }
+        int subtitulo = 0;
+        
+        String s = elements.get(0);
+        try {
+          if (s != null) {
+            subtitulo = Integer.parseInt(s);
+          }
+        } catch (Exception e) {
+          throw new ExtractionException(
+              "No es posible extraer el número de subtitulo");
+        }
+        
+        int item = 0;
+        s = elements.get(1);
+        if (s != null) {
+          try {
+            item = Integer.parseInt(s);
+          } catch (NumberFormatException e) {
+            ClasificadorPresupuestarioExtractor.log
+                .finest("No se pudo obtener un número de item");
+          }
+        } else {
+          item = 0;
+        }
+        
+        int asignacion = 0;
+        s = elements.get(2);
+        if (s != null) {
+          try {
+            asignacion = Integer.parseInt(s);
+          } catch (NumberFormatException e) {
+            ClasificadorPresupuestarioExtractor.log
+                .finest("No se pudo obtener un número de asignación");
+          }
+        }
+        
+        String nombre;
+        s = elements.get(3);
+        if (s != null) {
+          nombre = s;
+        } else {
+          throw new ExtractionException(
+              "No es posible extraer el nombre del elemento" + subtitulo + item
+                  + asignacion);
+        }
+        String descripcion;
+        s = elements.get(4);
+        if (s != null) {
+          descripcion = s;
+        } else {
+          descripcion = "";
+        }
+        
+        ClasificadorPresupuestario c = new ClasificadorPresupuestario(subtitulo,
+            item, asignacion, nombre, descripcion);
+        clasificadores.add(c);
+        cnumber++;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      
+    }
+    return clasificadores;
+  }
+  
+  /*
+   * (non-Javadoc)
+   * @see com.ingenium.goby.extractors.Extractor#getExtractionDestination()
+   */
+  @Override
+  public String getExtractionDestination() {
+    return extractionDestination;
+  }
+  
+  /*
+   * (non-Javadoc)
+   * @see com.ingenium.goby.extractors.Extractor#getExtractionSource()
+   */
+  @Override
+  public String getExtractionSource() {
+    return extractionSource;
+  }
+  
+  /**
+   * Obtiene un CVSReader apuntando al archivo de origen detallado en el archivo
+   * extractor.properties
+   *
+   * @return el reader
+   * @throws ExtractionException
+   *           en caso de que no pueda obtenerse el handler al reader
+   */
+  private CSVReader getReader() throws ExtractionException {
     FileInputStream fi;
     try {
       fi = new FileInputStream(extractionSource);
@@ -67,42 +251,6 @@ public class ClasificadorPresupuestarioExtractor implements Extractor {
       }
       throw new ExtractionException("Error de codificación del archivo fuente");
     }
-    
-    ClasificadorPresupuestarioExtractor.log.fine("Comenzando la extracción");
-
-    String[] line;
-    try {
-      while ((line = reader.readNext()) != null) {
-        StringBuilder stb = new StringBuilder(400);
-        for (String element : line) {
-          stb.append(element);
-          stb.append("\t");
-        }
-        System.out.println(stb);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-
-    }
-
+    return reader;
   }
-
-  /*
-   * (non-Javadoc)
-   * @see com.ingenium.goby.extractors.Extractor#getExtractionDestination()
-   */
-  @Override
-  public String getExtractionDestination() {
-    return extractionDestination;
-  }
-
-  /*
-   * (non-Javadoc)
-   * @see com.ingenium.goby.extractors.Extractor#getExtractionSource()
-   */
-  @Override
-  public String getExtractionSource() {
-    return extractionSource;
-  }
-
 }

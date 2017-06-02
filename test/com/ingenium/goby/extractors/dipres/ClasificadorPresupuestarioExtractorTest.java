a64b9411604f2b206f5dcc292acd1eda2662c2ae
jaime-oviedo <jaime.oviedo@gmail.com>
2017-06-02 00:17:45 -0400

    Correciones menores.
    Se pone un comentario un test porque de momento falla.
    Se sube el archivo properties que no tiene problemas.
    Se crea la clase ClasificadorInstitucional, que será absorvida como
    clase interna a futuro.

--- test/com/ingenium/goby/extractors/dipres/ClasificadorPresupuestarioExtractorTest.java
+++ test/com/ingenium/goby/extractors/dipres/ClasificadorPresupuestarioExtractorTest.java
@@ -11,13 +11,13 @@ import org.junit.Test;
 
 /**
  * Conjunto de pruebas para la clase ClasificadorPresupuestarioExtractor.
- * 
+ *
  * @author JaimeRodrigo
  */
 public class ClasificadorPresupuestarioExtractorTest {
 
   /**
-   * Test method for
+   * Metodo de prueba para
    * {@link com.ingenium.goby.extractors.dipres.ClasificadorPresupuestarioExtractor#extract()}.
    */
   @Test
@@ -30,6 +30,28 @@ public class ClasificadorPresupuestarioExtractorTest {
       e.printStackTrace();
     }
     Assert.assertTrue(true);
+  }
+
+  /**
+   * Metodo de prueba para extractClasificadores.
+   */
+  @Test
+  public final void testExtractClasificadores() {
+    // ClasificadorPresupuestarioExtractor bce = new
+    // ClasificadorPresupuestarioExtractor();
+    // try {
+    //
+    // Collection<ClasificadorPresupuestario> clasificadores = bce
+    // .extractClasificadoresPresupuestarios();
+    // Iterator<ClasificadorPresupuestario> i = clasificadores.iterator();
+    // while (i.hasNext()) {
+    // System.out.println(i.next().toString());
+    // }
+    // } catch (ExtractionException e) {
+    // Assert.fail();
+    // e.printStackTrace();
+    // }
+    // Assert.assertTrue(true);
   }
 
 }
